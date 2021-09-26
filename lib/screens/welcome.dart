cb579e2330499a7fe01f08f614788e3bf2c557d0
Supakrit6081139 <supakrit.nuc@student.mahidol.edu>
2021-09-26 15:12:58 +0700

    Implement FlashCard

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:language_hack/screens/home.dart';
+import 'package:flip_card/flip_card.dart';
 
 class WelcomeScreens extends StatefulWidget {
   const WelcomeScreens({Key? key}) : super(key: key);
@@ -33,6 +34,80 @@ class _WelcomeScreensState extends State<WelcomeScreens> {
                 style: TextStyle(fontSize: 40, color: Colors.black),
                 textAlign: TextAlign.left,
               )),
+            ),
+            Padding(padding: EdgeInsets.all(16.0)),
+            SizedBox(
+                width: 100,
+                height: 230,
+                child: FlipCard(
+                  front: Card(
+                    elevation: 4,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/bg.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Image.asset(
+                            'assets/logo.png',
+                            width: 100,
+                            height: 100,
+                          ),
+                          const Text(
+                            "Hello!",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 50, color: Colors.black),
+                          ),
+                          Padding(padding: EdgeInsets.all(5.0)),
+                          const Text(
+                            "Example Sentence: Hello, Jacob",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 20, color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  back: Card(
+                    elevation: 4,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/bg.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(padding: EdgeInsets.all(45.0)),
+                          const Text(
+                            "(noun) สวัสดี!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )),
+            Padding(padding: EdgeInsets.all(16.0)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                OutlinedButton.icon(
+                    onPressed: () => {},
+                    icon: Icon(Icons.chevron_left),
+                    label: Text("Prev")),
+                OutlinedButton.icon(
+                    onPressed: () => {},
+                    icon: Icon(Icons.chevron_right),
+                    label: Text("Next")),
+              ],
             ),
             Container(
               margin: EdgeInsets.only(top: 20, left: 20, right: 20),
